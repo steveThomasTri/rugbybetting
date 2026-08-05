4602c1260578e9f8e0d8ef8e3450c36cfb5d7f8b
Steve Thomas <stevethomasproject@gmail.com>
2026-08-05 06:29:37 -0400

    baccarrat adjustment

---
+++ extra/baccarat.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import sqlite3
+import os
+import time
+
+DB = "betting.db"
+db = sqlite3.connect(DB)
+
+#haz 1
+#henkel 3
+#blaze 5
+#enzo 6
+#trif 7
+for player in [[1,-10],[3,-10],[5,10],[6,10],[7,-10]]:
+    db.execute("""
+            UPDATE Players
+            SET Bank = Bank + ?
+            WHERE ID = ?
+        """, (player[1], player[0]))
+
+db.commit()
